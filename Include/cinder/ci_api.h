34d2cc34ec300d350a82db65fb342f85c2734aec
Itamar Oren <itamaro@meta.com>
2024-11-04 14:16:09 -0800

    Fix Windows build
    Summary:
    some code to support cinderx 3.12 was added, but in a way that broke the windows build. this diff fixes that by:
    1. using `Py_EXPORTED_SYMBOL` instead of `__attribute__ ((visibility ("default")))` (`Py_EXPORTED_SYMBOL` has all the platform-specific ifdefs to do the right thing)
    1. add the cinder hooks and ci_api headers and cinderhooks.c to the relevant MSVC project files to let the windows build find and include them when building
    
    Reviewed By: martindemello
    
    Differential Revision: D65397438
    
    fbshipit-source-id: 8a373d8c438f8b4cea5e1299f939fb17ef2dc46c

--- Include/cinder/ci_api.h
+++ Include/cinder/ci_api.h
@@ -10,25 +10,27 @@
  * prerequisites for CinderX being compatible with non-Cinder Python.
  */
 
+#include "exports.h"
+
 // These function the same as PyAPI_* - exporting symbols for use in .so's etc.
-#define CiAPI_FUNC(RTYPE) __attribute__ ((visibility ("default"))) RTYPE
+#define CiAPI_FUNC(RTYPE) Py_EXPORTED_SYMBOL RTYPE
 #ifdef __clang__
 #  ifdef __cplusplus
-#    define CiAPI_DATA(RTYPE) __attribute__ ((visibility ("default"))) extern "C" RTYPE
+#    define CiAPI_DATA(RTYPE) Py_EXPORTED_SYMBOL extern "C" RTYPE
 #  else
-#    define CiAPI_DATA(RTYPE) __attribute__ ((visibility ("default"))) extern RTYPE
+#    define CiAPI_DATA(RTYPE) Py_EXPORTED_SYMBOL extern RTYPE
 #  endif
 #else
 #  ifdef __cplusplus
-#    define CiAPI_DATA(RTYPE) extern "C" __attribute__ ((visibility ("default")))RTYPE
+#    define CiAPI_DATA(RTYPE) extern "C" Py_EXPORTED_SYMBOL RTYPE
 #  else
-#    define CiAPI_DATA(RTYPE) extern __attribute__ ((visibility ("default"))) RTYPE
+#    define CiAPI_DATA(RTYPE) extern Py_EXPORTED_SYMBOL RTYPE
 #  endif
 #endif
 
 // Clang seems to (always?) make symbols for static inline functions.
 #ifdef __clang__
-#  define CiAPI_STATIC_INLINE_FUNC(RTYPE) static inline __attribute__ ((visibility ("default"))) RTYPE
+#  define CiAPI_STATIC_INLINE_FUNC(RTYPE) static inline Py_EXPORTED_SYMBOL RTYPE
 #else
 #  define CiAPI_STATIC_INLINE_FUNC(RTYPE) static inline RTYPE
 #endif
