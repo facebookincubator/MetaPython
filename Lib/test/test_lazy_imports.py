c1033df5d73ca06c83f9a3044e3f93044536c4b3
Dino Viehland <dinoviehland@meta.com>
2025-02-27 05:02:11 -0800

    Make it easier to run a single lazy imports test
    Summary: Currently the lazy import tests are all sub-tests. These are difficult to run just a single test, so this makes them all of their own dynamically defined tests which can be run w/ cinder test runner.
    
    Reviewed By: alexmalyshev
    
    Differential Revision: D70130191
    
    fbshipit-source-id: e6c843932d9f6a326d0c1e4d2bfba4ae26eff50d

--- Lib/test/test_lazy_imports.py
+++ Lib/test/test_lazy_imports.py
@@ -6,7 +6,10 @@ import unittest
 
 
 class LazyImportsTest(unittest.TestCase):
-    def test_lazy_imports(self):
+    pass
+
+def make_test(test_name: str, lazy_imports: bool) -> object:
+    def test_case(self):
         original_lazy_modules = sys.lazy_modules.copy()
         original_modules = sys.modules.copy()
         try:
@@ -16,34 +19,30 @@ class LazyImportsTest(unittest.TestCase):
                 if modname == "test" or modname.startswith("test."):
                     del sys.modules[modname]
             stripped_modules = sys.modules.copy()
-            base = os.path.dirname(__file__)
-            tests = []
-            for path in os.listdir(os.path.join(base, "lazyimports")):
-                if path == "data" or path.startswith(("_", ".")):
-                    continue
-                if path.endswith(".py"):
-                    path = path[:-3]
-                test_name = "test.lazyimports." + path.replace(os.sep, ".")
-                tests.append(test_name)
-            for test_name in tests:
-                for lazy_imports in (True, False):
-                    msg = f"{test_name}{' (lazy)' if lazy_imports else ' (eager)'}"
-                    with self.subTest(msg=msg):
-                        self._test_name = test_name
-                        self._lazy_imports = lazy_imports
-                        previously = _imp._set_lazy_imports(lazy_imports)
-                        try:
-                            importlib.import_module(test_name)
-                        finally:
-                            _imp._set_lazy_imports(*previously)
-                            del self._test_name
-                            del self._lazy_imports
-                            sys.lazy_modules.clear()
-                            sys.modules.clear()
-                            sys.modules.update(stripped_modules)
-
+            msg = f"{test_name}{' (lazy)' if lazy_imports else ' (eager)'}"
+            self._test_name = test_name
+            self._lazy_imports = lazy_imports
+            previously = _imp._set_lazy_imports(lazy_imports)
+            try:
+                importlib.import_module(test_name)
+            finally:
+                _imp._set_lazy_imports(*previously)
+                del self._test_name
+                del self._lazy_imports
         finally:
             sys.lazy_modules.clear()
             sys.lazy_modules.update(original_lazy_modules)
             sys.modules.clear()
             sys.modules.update(original_modules)
+
+    return test_case
+
+base = os.path.dirname(__file__)
+for path in os.listdir(os.path.join(base, "lazyimports")):
+    if path == "data" or path.startswith(("_", ".")):
+        continue
+    path = path.removesuffix(".py")
+    for lazy_imports in (True, False):
+        test_name = "test_" + path.replace(os.sep, "_") + ("_lazy" if lazy_imports else "_eager")
+        test_mod = "test.lazyimports." + path.replace(os.sep, ".")
+        setattr(LazyImportsTest, test_name, make_test(test_mod, lazy_imports))
