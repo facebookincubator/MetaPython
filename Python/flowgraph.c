ccebbb433b86ccd2f3361e7421b9178e63db047f
Lukasz Langa <ambv@meta.com>
2026-06-11 13:20:49 -0700

    Import CPython 3.14.5+ stable branch (2026-06-09)
    Summary:
    Imported python/cpython `3.14.5+` from upstream rev [`51e83ca`](https://www.github.com/python/cpython/commit/51e83ca706d04878e0f6f0bf8030e0dd2f7585a6) (committed 2026-06-09 22:48:25+00:00).
    
    # Commit Info
    
    - Base: (`3.14.5+`) - [`d56f07c`](https://www.github.com/python/cpython/commit/d56f07c90008170c6eadec226955b3de29e849ea) (commit date: 2026-06-08 12:58:59+00:00)
    - Imported: (`3.14.5+`) - [`51e83ca`](https://www.github.com/python/cpython/commit/51e83ca706d04878e0f6f0bf8030e0dd2f7585a6) (commit date: 2026-06-09 22:48:25+00:00)
    
    # Noteworthy file changes
    
    - Native files (2 added):
      ```
      + Modules/_testcapi/weakref.c
      + Modules/_testlimitedcapi/weakref.c
      ```
    - Test files (1 added)
    - Low-signal files (14 added, 3 removed) (NEWS.d, docs, .github)
    
    Complete list of added/removed files: https://www.internalfb.com/intern/everpaste/?color=0&handle=GMci3SqBZT1xMMcFAGiA-UwAcm1sbr0LAAAz
    
    Reviewed By: itamaro
    
    Differential Revision: D108133995
    
    fbshipit-source-id: 2f69973b127d0430c08db8ece89952daa200cff0

--- Python/flowgraph.c
+++ Python/flowgraph.c
@@ -3182,6 +3182,7 @@ remove_unused_consts(basicblock *entryblock, PyObject *consts)
 
     index_map = PyMem_Malloc(nconsts * sizeof(Py_ssize_t));
     if (index_map == NULL) {
+        PyErr_NoMemory();
         goto end;
     }
     for (Py_ssize_t i = 1; i < nconsts; i++) {
@@ -3234,6 +3235,7 @@ remove_unused_consts(basicblock *entryblock, PyObject *consts)
     /* adjust const indices in the bytecode */
     reverse_index_map = PyMem_Malloc(nconsts * sizeof(Py_ssize_t));
     if (reverse_index_map == NULL) {
+        PyErr_NoMemory();
         goto end;
     }
     for (Py_ssize_t i = 0; i < nconsts; i++) {
