9719a1c8a60366353bbc9ff7846126b9a0407fa2
Thomas Wouters <twouters@meta.com>
2026-08-03 18:36:39 -0700

    Sync main's 03-add-parallel-gc-hooks patch with 3.15's.
    Reviewed By: itamaro
    
    Differential Revision: D114626314
    
    fbshipit-source-id: 578c8a81710a38519efc06f4297a5afb5aa72c30

--- Include/pystate.h
+++ Include/pystate.h
@@ -119,8 +119,10 @@ PyAPI_FUNC(void) PyGILState_Release(PyGILState_STATE);
 */
 PyAPI_FUNC(PyThreadState *) PyGILState_GetThisThreadState(void);
 
+#ifndef Py_LIMITED_API
 // Sets the thread state for multi-threaded GC
-PyAPI_FUNC(void) Ci_SetTStateForGC(PyThreadState *tstate);
+PyAPI_FUNC(void) _Ci_PySetTStateForGC(PyThreadState *tstate);
+#endif
 
 /* PEP 788 -- Protection against interpreter finalization */
 
