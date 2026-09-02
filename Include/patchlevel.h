66734c89ff486b161c03465da2041606bfafd358
generatedunixname1734921407115435 <noreply+1734921407115435@fb.com>
2026-09-02 02:55:19 -0700

    Sync pre-release CPython 3.15 branch from GitHub (2026-09-01)
    Summary:
    Imported python/cpython `3.15.0rc2+dev` from upstream rev [`e9585e2`](https://www.github.com/python/cpython/commit/e9585e23b82028e5fe9085a5f80f1eaccd51141d) (committed 2026-09-01 21:01:44+00:00).
    
    # Commit Info
    
    - Base: (`3.15.0rc1+dev`) - [`b93576a`](https://www.github.com/python/cpython/commit/b93576a2b66d98ec8fe2bf64de6fd27a7851e1ed) (commit date: 2026-08-31 19:05:08+00:00)
    - Imported: (`3.15.0rc2+dev`) - [`e9585e2`](https://www.github.com/python/cpython/commit/e9585e23b82028e5fe9085a5f80f1eaccd51141d) (commit date: 2026-09-01 21:01:44+00:00)
    
    # Noteworthy file changes
    
    - Low-signal files (1 added, 53 removed) (NEWS.d, docs, .github)
    
    Complete list of added/removed files: https://www.internalfb.com/intern/everpaste/?color=0&handle=GEIpVC_4rYoWi4kLAOt9_N2vh4k6br0LAAAz
    
    Differential Revision: D118409610
    
    fbshipit-source-id: 849ca1a78f0492fc8dc13461c66fd186db0172c9

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -24,10 +24,10 @@
 #define PY_MINOR_VERSION        15
 #define PY_MICRO_VERSION        0
 #define PY_RELEASE_LEVEL        PY_RELEASE_LEVEL_GAMMA
-#define PY_RELEASE_SERIAL       1
+#define PY_RELEASE_SERIAL       2
 
 /* Version as a string */
-#define PY_VERSION              "3.15.0rc1+meta"
+#define PY_VERSION              "3.15.0rc2+meta"
 /*--end constants--*/
 
 
